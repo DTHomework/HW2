535bb6fcb32a54ac5e6cc70a0125b22b2a12b0c9
albertopiovan <alby929@gmail.com>
2015-04-17 10:21:09 +0200

    Added normalization for Md
    Plus we cut the PDP to a dimension 4 vector

--- Es1_PDP.m
+++ Es1_PDP.m
@@ -10,7 +10,25 @@ title('Continuous PDP (T_{sample} = T)');
 %I'll now sample the "continuous" PDP
 Tc = (0.25*T);      %New sampling time
 tau = 0:Tc:5;
+samples = 4;
 PDP_sampled = exp(-tau/tau_rms)./tau_rms;
+PDP_sampled = [PDP_sampled(1:samples) zeros(1, length(PDP_sampled )-samples)] ;
 figure
 stem(tau, PDP_sampled, 'm');
+title('Sampled PDP (T_{sample} = Tc)');
+
+K = 3; %K in dB
+K = 10^(K/10); % K in linear
+
+Md = sum(PDP_sampled) 
+
+C = sqrt(K/(K+1));
+
+norm = Md/( 1 - C^2 ); % MdNorm = 1 - c^2
+
+MdNorm = Md/norm; %PDP normalized
+
+sum( MdNorm )+ C^2 
+figure
+stem(tau, PDP_sampled/norm, 'm');
 title('Sampled PDP (T_{sample} = Tc)');
