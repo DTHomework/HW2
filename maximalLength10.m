e77688b82876147a760d4cefc9d5a08a1ff70dad
gianlucaagresti <gianluca.agresti@gmail.com>
2015-04-19 14:54:14 +0200

    preso dalla voglia di far niente ho fatto un ml sequence con L=10

---
+++ maximalLength10.m
@@ -0,0 +1,15 @@
+function [ ml ] = maximalLength10()
+a = ones( 1, 11 );
+a(1) = 0;
+ml(1) = a(1);
+for l = 2 : 2^10-1
+    b = xor( a(8), a(11) )
+    for i = 11 : -1 : 2
+        a( i ) = a( i - 1 );
+    end
+    a(1) = b;
+    ml( l ) = a(1);
+    a
+end
+end
+
